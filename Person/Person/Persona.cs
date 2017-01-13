125152d2c45fc85d2fcd4cf8cd1008e47c5a7f4c
3f02 <3f02@Liceospalla.local>
2017-01-13 10:45:51 +0100

    Primo commit

---
+++ Person/Person/Persona.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class Persona
+    {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+
+        private int _anni;
+        public int anni
+        {
+            set
+            {
+                if (value < 0) value = 1;
+                _anni = value;
+            }
+
+            get { return _anni; }
+
+        }
+        public Persona(string nome, string cognome, int anni)
+        {
+        private string _codicefiscale;
+        public string codicefiscale { get { return nome + cognome + anni; } }
+
+        public string describe()
+        {
+            string output = "Nome: " + nome + "\r\n";
+            output += "Cognome: " + cognome + "\r\n";
+            output += "Anni: " + anni + "\r\n";
+
+            return output;
+
+        }
+    }
+    }
+
+}
